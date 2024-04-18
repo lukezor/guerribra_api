bd1b00d6aafb99736077a4dbc5c3c4247129399c
lukezor <gabrielferreirasana@gmail.com>
2024-04-18 20:29:11 -0300

    wip online time

--- functions.py
+++ functions.py
@@ -47,11 +47,20 @@ def update_online_time():
     players = data["world"]["online_players"]
     players.sort(reverse=True,key=lambda x: x["level"])
     for player in players:
-        if not all_players:
-            player["online_time"] = 0
-            
-        for single_player in all_players:
-            if player["name"] == single_player["name"]:
-                player["online_time"] = player["online_time"] + 15
-                break
-    return players
+        player["online_time"] = 0
+    if not all_players:
+        all_players = players
+    else:
+        for player in players:   
+            for single_player in all_players:
+                found = False
+                if player["name"] == single_player["name"]:
+                    single_player["online_time"] = single_player["online_time"] + 15
+                    found = True
+                    break
+            if not found:
+                print("DANDO POP NESSE AQUI:")
+                print(all_players)
+                print(player)
+                all_players.remove(player)
+    return all_players
